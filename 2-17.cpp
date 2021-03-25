52ace614cfb1a3bbe52551311846032342f05235
Pavel Kondratev <80323585+Spc777@users.noreply.github.com>
2021-03-25 14:36:24 +0400

    Add files via upload

---
+++ 2-17.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+   // a
+   cout << "VX YZ" << endl;
+
+   // b
+   cout << "V" << "X " << "Y" << "Z\n";
+
+   // c
+   cout << "V";
+   cout << "X YZ\n";
+
+   return 0;
+}
