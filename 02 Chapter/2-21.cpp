15f7865a05c543595274b13f526d743e2c05f806
Pavel Kondratev <80323585+Spc777@users.noreply.github.com>
2021-03-25 16:14:00 +0400

    Create 2-21.cpp

---
+++ 02 Chapter/2-21.cpp
@@ -0,0 +1,14 @@
+//Deitels C++ 10th edition exercises
+//Pavel Kondratev
+//Exercise 2.21
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+   cout << "  CCC\t+\t+\n C\t+\t+\nC     +++++   +++++\n C\t+\t+\n  CCC\t+\t+"
+      << endl;
+
+   return 0;
+}
