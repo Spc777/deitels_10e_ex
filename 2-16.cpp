03f4f666f71b496788c3fe1458a2fe6cd02af4af
Pavel Kondratev <80323585+Spc777@users.noreply.github.com>
2021-03-25 14:31:46 +0400

    Add files via upload

---
+++ 2-16.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    cout << "Please, input two numbers: ";
+    cin >> num1 >> num2;
+    cout << "the sum of entered numbers is: " << num1 + num2 << endl
+    << "the product of entered numbers is: " << num1 * num2 << endl
+    << "the difference of entered numbers is: " << num1 - num2 << endl;
+    if (num2 !=0)
+    cout << "the quotient of entered numbers is: " << num1 / num2;
+    else
+        cout << "you cannot divide by zero!";
+
+
+    return 0;
+}
