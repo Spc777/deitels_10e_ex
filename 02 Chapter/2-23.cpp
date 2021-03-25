690c6cf29eb86b36dead0e77eddc34a309162822
Pavel Kondratev <80323585+Spc777@users.noreply.github.com>
2021-03-25 16:23:36 +0400

    Create 2-23.cpp

---
+++ 02 Chapter/2-23.cpp
@@ -0,0 +1,34 @@
+//Deitels C++ 10th edition exercises
+//Pavel Kondratev
+//Exercise 2.23
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+   int n1, n2, n3, n4, n5;
+   
+   cout << "Input five integer numbers: ";
+   cin >> n1 >> n2 >> n3 >> n4 >> n5;
+
+   int max{n1};
+
+   if ( n2 > max ) max = n2;
+   if ( n3 > max ) max = n3;
+   if ( n4 > max ) max = n4;
+   if ( n5 > max ) max = n5;
+
+   int min{n1};
+
+   if ( n2 < min ) min = n2;
+   if ( n3 < min ) min = n3;
+   if ( n4 < min ) min = n4;
+   if ( n5 < min ) min = n5;
+
+   cout << "largest is " << max
+      << "\nSmallest is " << min << endl;
+
+   return 0;
+}
